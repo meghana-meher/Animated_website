67f9ae9687613684cf9c6bc86c3968b87f165040
meghana-meher <meghanameher.tenneti@gmail.com>
2025-08-04 18:01:00 +0530

    animation

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
-// gsap timeline
+// gsap banner timeline
 var tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".part-1",
@@ -82,3 +82,195 @@ tl.to(
   },
   "a"
 );
+
+// rounded div
+var tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".part-2",
+    start: "0% 70%",
+    end: "50% 50%",
+    scrub: true,
+    // markers: true,
+  },
+});
+
+tl2.to(".rounded-div-wrapper", {
+  height: 0,
+  marginTop: 0,
+});
+
+// text-animation
+
+let tl3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".content-2",
+    start: "20% 50%",
+    end: "100% 50%",
+    // markers: true,
+    scrub: 1,
+  },
+});
+tl3.to(".content-2 .text-area-hover h1", {
+  width: "100%",
+});
+tl3.to(".content-2 .text-area-hover h2", {
+  delay: -0.4,
+  width: "100%",
+});
+
+// part-4
+
+let tl4 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".part-4",
+    start: "50% 50%",
+    end: "200% 50%",
+    pin: true,
+    // markers: true,
+    scrub: 1,
+  },
+});
+
+tl4.to(
+  ".c-one",
+  {
+    marginTop: "-25%",
+    opacity: "1",
+  },
+  "sct-1"
+);
+tl4.to(
+  ".c-two",
+  {
+    opacity: "1",
+  },
+  "sct-2"
+);
+tl4.to(
+  ".c-one",
+  {
+    marginTop: "-100",
+    opacity: "0",
+  },
+  "sct-2"
+);
+tl4.to(
+  ".c-three",
+  {
+    opacity: "1",
+  },
+  "sct-3"
+);
+tl4.to(
+  ".c-two",
+  {
+    opacity: "0",
+  },
+  "sct-3"
+);
+tl4.to(
+  ".c-one",
+  {
+    marginTop: "-180%",
+  },
+  "sct-3"
+);
+tl4.to(
+  ".c-one",
+  {
+    marginTop: "-230%",
+  },
+  "sct-4"
+);
+tl4.to(
+  ".c-three",
+  {
+    opacity: "0",
+  },
+  "sct-4"
+);
+tl4.to(
+  ".cir-part-4",
+  {
+    marginLeft: "100%",
+    rotate: 360,
+  },
+  "sct-4"
+);
+
+// part5
+let tl5 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".part-5",
+    start: "20% 50%",
+    end: "100% 50%",
+    // markers: true,
+    scrub: 1,
+  },
+});
+tl5.to(".part-5 .text-area-hover h1", {
+  width: "100%",
+});
+tl5.to(".part-5 .text-area-hover h2", {
+  delay: -0.4,
+  width: "100%",
+});
+
+let tl6 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".part-6",
+    start: "0% 70%",
+    end: "15% 50%",
+    // markers: true,
+    scrub: 1,
+  },
+});
+tl6.to(".rounded-div-wrapper-6", {
+  height: "0%",
+  marginTop: 0,
+});
+
+let tl7 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".part-7",
+    start: "50% 50%",
+    end: "300% 50%",
+    pin: true,
+    // markers: true,
+    scrub: 1,
+  },
+});
+tl7.to("#demo", {
+  bottom: "7%",
+});
+tl7.to(
+  ".our-work-txt-div",
+  {
+    height: "60vh",
+  },
+  "height"
+);
+tl7.to(
+  ".our-work-txt",
+  {
+    height: "60vh",
+  },
+  "height"
+);
+tl7.to(
+  "#our",
+  {
+    left: "0%",
+  },
+  "height"
+);
+tl7.to(
+  "#work",
+  {
+    right: "0%",
+  },
+  "height"
+);
+tl7.to(".scroll-img", {
+  marginTop: "-300%",
+});
